cc241e4dbe43381a6179012476988965cfb9382b
HGD7764 <65435258+HGD7764@users.noreply.github.com>
2025-05-24 18:58:30 +0800

    Add files via upload

---
+++ get.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'https://github.com/wzdnzd/aggregator/issues/91'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+class1 = soup.find_all('td', class_='notranslate')
+print(class1)
